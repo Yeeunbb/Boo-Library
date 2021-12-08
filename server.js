c5da6f28adb705f76ab512e69e6c446b60076ac9
YEEUN <ykoou@naver.com>
2021-12-08 22:07:32 +0900

    Edit server.js

--- server.js
+++ server.js
@@ -198,6 +198,6 @@ async function setBooType(nickname, btype){
 
 // const hostname = '127.0.0.1';
 const port = 3000;
-server.listen(port, () => {
+server.listen(process.env.PORT || port, () => {
   console.log("app is running on port " + port);
 });
