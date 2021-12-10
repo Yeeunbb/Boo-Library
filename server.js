f5b31553e4bc0794ae9050dcb29a453a1e4e5756
YEEUN <ykoou@naver.com>
2021-12-10 21:35:22 +0900

    Add Recomend Book Page

--- server.js
+++ server.js
@@ -6,55 +6,38 @@ const ejs = require('ejs');
 const app = express();
 const server = http.createServer(app);
 
+//DB 정보
 var serviceAccount = require("./boo-library-firebase-adminsdk-1girc-5b4078cab2.json");
 var admin = require("firebase-admin");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 const db = admin.firestore();
+
+//Naver 검색 api
 const NAVER_CLIENT_ID = '1NocLRDOeeZo5fptyb4n';
 const NAVER_CLIENT_SECRET = 'QSZvjQ6x12';
-const option = {
-  query : '소크라테스의 변명', //검색어
-  display : 5, //가져올 검색 결과 수
-  start : 1,
-
-}
 const request = require('request');
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }))
+//브라우저 쿠키 사용
 app.use(session({
   secret:"qwef!#@#12opiozcxz^",
   resave: false,
   saveUninitialized : true
 }));
 
+//mainPage
 app.get('/', (req, res) => {
-  // request.get({
-  //   uri: 'https://openapi.naver.com/v1/search/book.json',
-  //   qs : option,
-  //   headers: {
-  //     'X-Naver-Client-Id' : NAVER_CLIENT_ID,
-  //     'X-Naver-Client-Secret' : NAVER_CLIENT_SECRET
-  //   }
-  // }, function(err, res, body){
-  //   let json = JSON.parse(body);
-  //   for(var book in json.items){
-  //     console.log(json.items[book].title);
-  //   }
-  //   // console.log(json.items[0].title);
-  // });
-    
-  if(req.session.isLogined == true)
-    return res.render('myBoo', {
+  if(req.session.isLogined == true) //로그인 한 사용자의 경우
+    return res.render('mainPage', {
       boo : req.session.booType
     });
   else
-    return res.render('index');
-  // res.render('index');
+    return res.render('index'); //첫 방문인 경우
 });
 app.get('/startTest', (req, res) => {
   res.render('startTest');
@@ -67,8 +50,15 @@ app.get('/afterTest', (req, res) => {
 });
 app.get('/myBoo', (req, res) => {
   res.render('myBoo', {
-    boo : req.session.booType
+    boo : req.session.booType,
+    nick : req.session.nickName
   });
+});
+app.get('/Main', (req, res) => {
+  res.render('mainPage');
+});
+app.get('/Search', (req, res) => {
+  res.render('bookSearch');
 });
 
 //userInfo form 제출 (POST)
@@ -95,7 +85,7 @@ app.post('/Intro', async function(req, res) {
       check = true;
     });
 
-  const nickRef = db.collection('nickName').doc(nick);
+  const nickRef = db.collection('userInfo').doc(nick);
   const doc = await nickRef.get();
   if(!doc.exists){
     //없는 닉네임. 가입 진행
@@ -170,18 +160,56 @@ app.post('/testRes', async function(req, res) {
     booType = 'summer';
   }
 
-  setBooType('예니3', booType);
+  setBooType(req.session.nickName, booType);
   req.session.booType = booType;
   res.json({msg: booType});
 });
 
 
-async function updateData(nickname, major, email) {
-  const userUD = await db.collection('dataByMajor').doc(major).set({
-    like : 0,
-    dislike : 0,
+//책 이름으로 검색하기
+app.post('/searchBook', async function(req, res) {
+  var bookName = req.body.book;
+  console.log("book Title: ", bookName);
+
+  const option = {
+    query : bookName, //검색어
+    display : 10, //가져올 검색 결과 수
+    start : 1,
+  }
+
+  request.get({
+    uri: 'https://openapi.naver.com/v1/search/book.json',
+    qs : option,
+    headers: {
+      'X-Naver-Client-Id' : NAVER_CLIENT_ID,
+      'X-Naver-Client-Secret' : NAVER_CLIENT_SECRET
+    }
+      }, function(err, res, body){
+          let bookInfos = JSON.parse(body);
+          var dataList = new Array();
+          //console.log(json);
+
+          for(var book in bookInfos.items){
+            //console.log(json.items[book].title);
+            var data = new Object();
+            data.title = bookInfos.items[book].title;
+            data.image = bookInfos.items[book].image; 
+            data.author = bookInfos.items[book].author; 
+            data.publisher = bookInfos.items[book].publisher; 
+            data.pubdate = bookInfos.items[book].pubdate; 
+            
+            dataList.push(data);
+       }
+       var jsonData = JSON.stringify(dataList); 
+       console.log(jsonData);
+    // console.log(json.items[0].title);
   });
-  const nickUD = await db.collection('nickName').doc(nickname).set({
+});
+
+
+
+async function updateData(nickname, major, email) {
+  const userRef = await db.collection('userInfo').doc(nickname).set({
     nickName : nickname,
     major : major,
     email : email,
@@ -191,7 +219,7 @@ async function updateData(nickname, major, email) {
 }
 
 async function setBooType(nickname, btype){
-  const userRef = db.collection('nickName').doc(nickname);
+  const userRef = db.collection('userInfo').doc(nickname);
   const res = await userRef.update({'booType' : btype});
 }
 
@@ -201,3 +229,4 @@ const port = 3000;
 server.listen(process.env.PORT || port, () => {
   console.log("app is running on port " + port);
 });
+//heroku 배포시 port를 지정하면 안됨.
